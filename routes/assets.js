c5df245122a3fcf501548caaf41d10005da20e6f
Shea <120745822@qq.com>
2025-07-30 06:48:25 +0000

    functions

--- routes/assets.js
+++ routes/assets.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/assetsController');
 
-router.post('/', controller.createAsset);
-router.get('/account/:account_id', controller.getAssetsByAccount);
-router.put('/:asset_id', controller.updateAsset);
-router.delete('/:asset_id', controller.deleteAsset);
+// router.post('/', controller.createAsset);
+router.get('/type/:type_name', controller.getAssetsByType); //  ('stock'), ('bonds'), ('cash'), ('others');
+router.put('/:asset_name', controller.updateAsset);
+// router.delete('/:asset_id', controller.deleteAsset);
 
 module.exports = router;
--- routes/transactions.js
+++ routes/transactions.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/transactionsController');
 
 router.post('/', controller.createTransaction);
-router.get('/asset/:asset_id', controller.getTransactionsByAsset);
+// router.get('/asset/:asset_id', controller.getTransactionsByAsset);
+router.get('/', controller.getTransactions);
+
 
 module.exports = router;
