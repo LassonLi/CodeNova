f541d513857e3c3d365aa78434aefc7b3afba006
eric <erichuangemail@163.com>
2025-07-30 02:20:40 +0000

    insert data sql

---
+++ config/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO assets (
+account_id, asset_type_id, asset_name,
+current_quantity, current_price_per_unit,
+purchase_price, average_price, total_amount
+) VALUES
+(1, 1, 'AAPL', 50.00, 195.32, 190.00, 192.50, 9625.00),
+(1, 1, 'GOOGL', 10.00, 2842.10, 2700.00, 2771.05, 27710.50),
+(1, 2, 'BTC', 0.25, 29650.20, 29000.00, 29325.10, 7331.28);
